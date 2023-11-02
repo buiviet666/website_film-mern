9205d9fc1ef3d840072caac4c7c885edef1e6c01
buiviet666 <loverbee01@gmail.com>
2023-11-02 17:22:22 +0700

    update client

--- server/index.js
+++ server/index.js
@@ -4,24 +4,27 @@ import cors from "cors";
 import http from "http";
 import mongoose from "mongoose";
 import "dotenv/config";
+import routes from "./src/routes/index.js";
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-const port = process.env.PORT || 3000
+app.use("/api/v1", routes);
 
-const server = http.createServer(app)
+const port = process.env.PORT || 5000;
+
+const server = http.createServer(app);
 
 mongoose.connect(process.env.MONGODB_URL).then(() => {
-    console.log("mongodb connected");
+    console.log("Mongodb connected");
     server.listen(port, () => {
-        console.log(`server open on port ${port}`);
-    })
+        console.log(`Server is listening on port ${port}`);
+    });
 }).catch((err) => {
     console.log({ err });
     process.exit(1);
-})
+});

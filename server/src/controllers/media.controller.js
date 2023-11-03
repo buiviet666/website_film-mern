7156c02c1da559b63443712ec4250516d9ebd371
buiviet666 <loverbee01@gmail.com>
2023-11-03 15:05:09 +0700

    update but not ok

--- server/src/controllers/media.controller.js
+++ server/src/controllers/media.controller.js
@@ -1,14 +1,13 @@
-import responseHandler from "../handlers/response.handler";
-import tmdbApi from "../tmdb/tmdb.api";
-import userModel from "../models/favorite.model";
-import favoriteModel from "../models/favorite.model";
-import reviewModel from "../models/review.model";
-import tokenMiddleware from "../middlewares/token.middleware";
+import responseHandler from "../handlers/response.handler.js";
+import tmdbApi from "../tmdb/tmdb.api.js";
+import userModel from "../models/user.model.js";
+import favoriteModel from "../models/favorite.model.js";
+import reviewModel from "../models/review.model.js";
+import tokenMiddlerware from "../middlewares/token.middleware.js";
 
 const getList = async (req, res) => {
     try {
         const { page } = req.query;
-
         const { mediaType, mediaCategory } = req.params;
 
         const response = await tmdbApi.mediaList({ mediaType, mediaCategory, page });
@@ -34,7 +33,6 @@ const getGenres = async (req, res) => {
 const search = async (req, res) => {
     try {
         const { mediaType } = req.params;
-
         const { query, page } = req.query;
 
         const response = await tmdbApi.mediaSearch({
@@ -51,43 +49,42 @@ const search = async (req, res) => {
 
 const getDetail = async (req, res) => {
     try {
-        const { mediaType, mediaId } = res.params;
+        const { mediaType, mediaId } = req.params;
 
         const params = { mediaType, mediaId };
 
         const media = await tmdbApi.mediaDetail(params);
 
-        media.creadits = await tmdbApi.mediaCredits(params);
+        media.credits = await tmdbApi.mediaCredits(params);
 
         const videos = await tmdbApi.mediaVideos(params);
+
         media.videos = videos;
 
         const recommend = await tmdbApi.mediaRecommend(params);
+
         media.recommend = recommend.results;
 
         media.images = await tmdbApi.mediaImages(params);
 
-        const tokenDecoded = tokenMiddleware.tokenDecode(req);
+        const tokenDecoded = tokenMiddlerware.tokenDecode(req);
 
         if (tokenDecoded) {
             const user = await userModel.findById(tokenDecoded.data);
+
             if (user) {
                 const isFavorite = await favoriteModel.findOne({ user: user.id, mediaId });
                 media.isFavorite = isFavorite !== null;
             }
         }
 
-        media.reviews = await reviewModel.find({ mediaId }).popular("user").sort("-createAt");
+        media.reviews = await reviewModel.find({ mediaId }).populate("user").sort("-createdAt");
 
         responseHandler.ok(res, media);
-    } catch {
+    } catch (e) {
+        console.log(e);
         responseHandler.error(res);
     }
 };
 
-export default {
-    getList,
-    getGenres,
-    search,
-    getDetail
-};
+export default { getList, getGenres, search, getDetail };
--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,18 +1,18 @@
-import userModel from "../models/user.model";
+import userModel from "../models/user.model.js";
 import jsonwebtoken from "jsonwebtoken";
-import responseHandler from "../handlers/response.handler";
+import responseHandler from "../handlers/response.handler.js";
 
 const signup = async (req, res) => {
     try {
-        const { username, password, displayname } = req.body;
+        const { username, password, displayName } = req.body;
 
         const checkUser = await userModel.findOne({ username });
 
-        if (checkUser) return responseHandler.badrequest(res, "user already used");
+        if (checkUser) return responseHandler.badrequest(res, "username already used");
 
         const user = new userModel();
 
-        user.displayname = displayname;
+        user.displayName = displayName;
         user.username = username;
         user.setPassword(password);
 
@@ -21,7 +21,7 @@ const signup = async (req, res) => {
         const token = jsonwebtoken.sign(
             { data: user.id },
             process.env.TOKEN_SECRET,
-            { expiresIn: "24h" },
+            { expiresIn: "24h" }
         );
 
         responseHandler.created(res, {
@@ -38,16 +38,16 @@ const signin = async (req, res) => {
     try {
         const { username, password } = req.body;
 
-        const user = await userModel.findOne({ username }).select("username password salt id displayname");
+        const user = await userModel.findOne({ username }).select("username password salt id displayName");
 
-        if (!user) return responseHandler.badrequest(res, "user not exist");
+        if (!user) return responseHandler.badrequest(res, "User not exist");
 
-        if (!user.validPassword(password)) return responseHandler.badrequest(res, "wrong password");
+        if (!user.validPassword(password)) return responseHandler.badrequest(res, "Wrong password");
 
         const token = jsonwebtoken.sign(
             { data: user.id },
             process.env.TOKEN_SECRET,
-            { expiresIn: "24h" },
+            { expiresIn: "24h" }
         );
 
         user.password = undefined;
@@ -69,9 +69,9 @@ const updatePassword = async (req, res) => {
 
         const user = await userModel.findById(req.user.id).select("password id salt");
 
-        if (!user) return responseHandler.unathorize(res);
+        if (!user) return responseHandler.unauthorize(res);
 
-        if (!user.validPassword(password)) return responseHandler.badrequest(res, "wrong password");
+        if (!user.validPassword(password)) return responseHandler.badrequest(res, "Wrong password");
 
         user.setPassword(newPassword);
 
@@ -88,6 +88,8 @@ const getInfo = async (req, res) => {
         const user = await userModel.findById(req.user.id);
 
         if (!user) return responseHandler.notfound(res);
+
+        responseHandler.ok(res, user);
     } catch {
         responseHandler.error(res);
     }
@@ -96,6 +98,6 @@ const getInfo = async (req, res) => {
 export default {
     signup,
     signin,
-    updatePassword,
-    getInfo
+    getInfo,
+    updatePassword
 };
